cccdcca5e975085c695c2c1ae881a4e494a4e30d
Tom Ridge <tom.j.ridge@googlemail.com>
2017-07-05 19:36:48 +0100

    +

--- src/object_map.ml
+++ src/object_map.ml
@@ -255,41 +255,64 @@ module File_ops = struct
 
   (* t is the root block of the idx_map; TODO this should make sure
      not to read beyond the end of file *)
+
+(*
+
+* pread specification
+
+- dst is modified from dst_pos
+- no more than len' <= len bytes are modified
+- the contents of dst from dst_pos matches src from src_pos (which
+  implies that src_pos+len' <= src_length)
+- len' is returned in the monad
+- len > 0 --> len' > 0
+
+This involves being able to reconstruct src from the idx->blk_id map
+and read_block.
+
+
+*)
+
+
   let pread'
       ~read_block 
       ~(find_leaf: 'k -> 'r -> ('r*('k*'v)list*('k,'r)rstk,'t) m)
       ~page_ref_ops
-      ~src_pos ~len ~dst ~dst_pos 
+      ~src_length ~src_pos ~len ~dst ~dst_pos 
     = (
+      assert (src_pos+len <= src_length);
       assert (dst_pos + len <= Bytes.length dst);
       (* read the relevant leaf *)
       let blk_i = src_pos / blk_sz in
       page_ref_ops.get () |> bind (fun r ->
         find_leaf blk_i r |> bind (fun (_,kvs,rstk) ->
           (* kvs is the map from idx -> block_id; rstk tells us the
-             maximum block we can try to read *)
-          (* restrict len so that we do not try to read past u *)
+             maximum block we can try to read; restrict len so that we
+             do not try to read past u; NOTE that u is strictly
+             greater than blk_i *)
           let (_,u) = stack_to_lu_of_child rstk in
           (* do not attempt to read block u or higher *)
           let limit_blk = match u with 
-            | None -> max_int
+            | None -> max_int  (* assume no file anywhere near max_int blocks *)
             | Some i -> i
-          in
+          in          
           (* convert kvs to map for ease of use *)
           let map = assoc_list_to_bst kvs in
+          let empty_blk = lazy (Block.of_string blk_sz "") in
           let rec loop ~src_pos ~len ~dst_pos ~n_read = (
-            let blk = src_pos / blk_sz in
+            let blk_i = src_pos / blk_sz in
             match () with
-            | _ when len=0 || blk >= limit_blk -> return n_read
+            | _ when len=0 || blk_i >= limit_blk -> return n_read
             | _ -> 
               let blk_offset = src_pos mod blk_sz in
               let get_blk = 
                 (* which block to read? *)
-                match map blk with
-                | None -> return (Block.of_string blk_sz "")
+                match map blk_i with
+                | None -> return (Lazy.force empty_blk)
                 | Some i -> read_block i
               in
               get_blk |> bind (fun blk -> 
+                (* NOTE len > 0 and 0 <= blk_offset < blk_sz *)
                 let len' = min len (blk_sz - blk_offset) in
                 block_buf_blit ~blk_sz ~src:blk ~src_pos:blk_offset 
                   ~len:len' ~dst ~dst_pos;
@@ -298,11 +321,29 @@ module File_ops = struct
           in
           loop ~src_pos ~len ~dst_pos ~n_read:0)))
 
+  let _ = pread'
+  
+
   (* pwrite is similar to pread; we aim for insert_many-like
      behaviour; we don't want to allocate all blocks up front; so
      insert_many needs to take a function that can be stepped and can
      produce a block or indicate that it is finished; for partial
      blocks, this needs access to read_block  *)
+
+(*
+
+* pwrite specification
+
+- dst is modified from dst_pos
+- no more than len' <= len bytes are modified
+- dst@dst_pos, len' bytes matches src@src_pos len' bytes (obviously
+  src is unaltered; dst file size may be increased)
+- (the abstraction from the btree to the file contents is wellformed;
+  the root ref is updated; btree invariants preserved)
+
+
+*)
+
   let insert_many ~(k:'k) ~(v:'v) ~(ks:'k list) ~(vs:'k -> blk) : ('k list,S.t) m = failwith "" 
     
   let pwrite'
@@ -310,12 +351,13 @@ module File_ops = struct
       ~max_leaf_keys
       ~read_block
       ~write_block
-      ~page_ref_ops
-      ~(insert_many: ('k*'v) list -> ('k,'r)rstk -> ('r,'t) m)
-      ~(find_leaf: 'k -> 'r -> ('r*('k*'v)list*('k,'r)rstk,'t) m)
+      ~page_ref_ops  (* for B-tree root *)
+      ~dst_size_ops  (* for dst size if writing beyond end *)
+      ~(insert_many_up: ('k*'v) list -> ('k,'r)rstk -> ('r,'t) m)  (* upwards phase of insert_many; may split initial leaf *)
+      ~(find_leaf: 'k -> 'r -> ('r*('k*'v)list*('k,'r)rstk,'t) m)  (* locate initial leaf holding the blk_i of the blk we first modify *)
       ~src ~src_pos ~len ~dst_pos : (int,S.t) m 
     = (
-      assert (src_pos + len <= Bytes.length src);    
+      assert (src_pos + len <= Bytes.length src);
       (* for dst, file size can grow *)
       begin
         let blk_i = dst_pos / blk_sz in
@@ -334,20 +376,30 @@ module File_ops = struct
             let rec loop ~src_pos ~len ~dst_pos ~n_wrote ~kvs = (
               let blk_i = src_pos / blk_sz in
               match () with
-              | _ when len=0 || blk_i >= limit_blk || List.length kvs >= 2*max_leaf_keys -> (
-                  (* now execute the up stage of insert_many to get a new page_ref *)
-                  insert_many kvs rstk |> bind (fun r ->
-                    page_ref_ops.set r |> bind (fun () ->
-                      return n_wrote)))
+              | _ when 
+                  len=0 || (* nothing left to write *)
+                  blk_i >= limit_blk || (* can't insert beyond the context bound *)
+                  List.length kvs >= 2*max_leaf_keys (* can't have a leaf too large *)
+                -> (  
+                    (* now execute the up stage of insert_many to get a new page_ref *)
+                    insert_many_up kvs rstk |> bind (fun r ->
+                      page_ref_ops.set r |> bind (fun () ->
+                        (* and update the size *)
+                        dst_size_ops.get () |> bind (fun dst_size -> 
+                          let new_size = max dst_size (dst_pos+n_wrote) in
+                          (match new_size > dst_size with
+                           | true -> dst_size_ops.set new_size | false -> return ())
+                          |> bind (fun () -> return n_wrote)))))
               | _ -> 
                 let blk_offset = src_pos mod blk_sz in
-                (* we may not be writing a full block *)
+                (* we may not be writing a full block; NOTE len > 0
+                   and 0 <= blk_offset < blk_sz and len' <= blk_sz *)
                 let len' = min len (blk_sz - blk_offset) in
                 let blk = 
                   match blk_offset > 0 || len' < blk_sz with
                   | true -> (
-                      (* a partial block write *)
-                      (* read the original block *)
+                      (* a partial block write; read the original
+                         block *)
                       (match map blk_i with
                        | None -> return (Block.of_string blk_sz "")
                        | Some i -> read_block i)
@@ -360,6 +412,8 @@ module File_ops = struct
                   | false -> (
                       Block.of_string blk_sz "" 
                       |> fun dst -> 
+                      (* NOTE blk_offset = 0 and len' >= blk_sz ie len' = blk_sz *)
+                      assert (blk_offset = 0 && len' = blk_sz);
                       buf_block_blit ~blk_sz ~src ~src_pos ~len:blk_sz ~dst ~dst_pos:0 
                       (* FIXME inefficient create of initial block *)
                       |> return)
@@ -384,38 +438,3 @@ let omap_ops : omap_ops = failwith ""
 
 (* TODO instantiate the omap with cache *)
 
-
-(*
-    calc ~pos:dst_pos ~len:src_len (fun ~blk_index ~offset ~len:src_len -> 
-
-        (* we can only write individual blocks to the lower layer... *)
-        (* we may have to read a block if offset <> 0 or src_len < blk_sz *)
-        let blk' = (
-          match (offset<>0 || src_len < blk_sz) with
-          | true -> (
-              map_ops.find blk_index |> bind (fun x -> 
-                  match x with 
-                  | None -> return (BlkN.of_string blk_sz "")
-                  | Some blk -> return blk))
-          | false -> (return (BlkN.of_string blk_sz "")))
-        in
-        blk' |> bind (fun blk' -> 
-            let blk' = blk_to_buffer blk' in
-
-            (* blk' is the block that we are preparing; at this point, if
-               needed it contains the data that was already present at
-               blk_index; now we blit the new bytes *)
-
-            (* FIXME we probably want to ensure that blks are
-               immutable, but at the same time minimize copying *)
-
-            blit ~src:src ~src_pos:src_pos ~dst:blk' ~dst_pos:offset ~len:src_len;
-            let blk' = buffer_to_blk blk' in
-            map_ops.insert blk_index blk' |> bind (fun _ -> 
-
-                (* we may have to adjust the size of the bfile; but
-                   clearly we want to avoid reading and writing the
-                   meta block if we can; *)
-                failwith "FIXME" |> bind (fun _ -> return src_len)))))
-
- *)
