fac3ae0bd4853a7677871fd74922c24b4cf7c7f1
Tom Ridge <tom.j.ridge@googlemail.com>
2018-07-04 16:23:50 +0100

    +

--- build_parent.sh
+++ build_parent.sh
@@ -6,7 +6,7 @@
 
 DST=..
 
-for f in isa_btree tjr_monad tjr_lib tjr_fs_shared tjr_btree path_resolution tjr_net tjr_pcache mini-fs; do
+for f in a_tjr_lib b_isa_btree b_tjr_fs_shared b_tjr_monad c_path_resolution c_tjr_btree c_tjr_net d_mini-fs d_tjr_pcache; do
     make -C $DST/$f clean 
     make -C $DST/$f || exit -1
 done
