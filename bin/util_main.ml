4919d7eae2f2b6037a219485d8284640da9a96b9
Tom Ridge <tom.j.ridge@googlemail.com>
2021-02-11 15:13:04 +0000

    remove src-test; remove docs

--- bin/util_main.ml
+++ bin/util_main.ml
@@ -4,6 +4,9 @@ let _ =
   match argv with
   | ["print_trace";dirname] -> 
     Test_blk_dev.print_trace ~dirname
+  | ["test";"file_impl_v2"] -> 
+    let module X = File_impl_v2.Test() in
+    Lwt_main.run (X.run () |> With_lwt.to_lwt)
   | ["test";"sqlite_dir"] -> 
     let module X = Sqlite_dir.Test() in
     Lwt_main.run (X.test_program |> Tjr_monad.With_lwt.to_lwt)
--- src-test/test.ml
+++
@@ -1,9 +0,0 @@
-(*
-let _ = 
-  File_impl_v1.Iter_block_blit.test ();
-  File_impl_v1.test ()
-*)
-
-module X = File_impl_v2.Test()
-
-let _ = Lwt_main.run (X.run () |> With_lwt.to_lwt)
