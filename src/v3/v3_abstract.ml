b8c40efbc29879c1d0ffdbf28ab59ba84f9b6536
Tom Ridge <tom.j.ridge@googlemail.com>
2021-02-19 19:22:19 +0000

    +

--- src/v3/v3_abstract.ml
+++ src/v3/v3_abstract.ml
@@ -1,8 +1,37 @@
 (** Abstract development of V3.
 
-See {!module:V3}. *)
+See {!module:V3}.
+
+Initial path resolution is carried out without locks. After, we lock
+   objects that we need to, validate the entries, and then go ahead
+   with the modifications. If we detect changes between path res and
+    execution, we backout with a concurrent modification error.
+
+*)
 
 type times = Minifs_intf.times
+
+
+module Errors = struct
+  (** Typical errors we encounter *)
+  type exn_ = [ 
+    | `Error_no_entry
+    | `Error_not_directory
+    | `Error_not_file
+    | `Error_not_symlink
+    | `Error_attempt_to_rename_dir_over_file
+    | `Error_attempt_to_rename_root
+    | `Error_attempt_to_rename_to_subdir
+    | `Error_no_src_entry
+    | `Error_path_resolution
+    | `Error_not_empty
+    | `Error_exists
+    | `Error_is_directory
+    | `Error_concurrent_modification (* new, compared to minifs *)
+    | `Error_other
+  ] [@@deriving bin_io, yojson]
+end
+open Errors
 
 
 (** NOTE The following are copied from v1_generic.ml *)
@@ -50,9 +79,10 @@ module Lock_ops = struct
   type ('tid,'id,'t) lock_ops = {
     lock       : tid:'tid -> objs:'id list -> (unit,'t)m;
     unlock     : tid:'tid -> objs:'id list -> (unit,'t)m;
-    unlock_all : tid:'tid -> (unit,'t)m;
+    (* unlock_all : tid:'tid -> (unit,'t)m; *)
   }
 end
+open Lock_ops
 
 module type S0 = sig
   type t
@@ -82,33 +112,22 @@ module S1(S0:S0) = struct
   type dir_v = dir_entry
 
   type dir_ops = {
-    find      : str_256 -> (dir_entry option,t)m;
-    insert    : str_256 -> dir_entry -> (unit,t)m;
+    find      : did:did -> str_256 -> (dir_entry option,t)m;
+    insert    : did:did -> str_256 -> dir_entry -> (unit,t)m;
+    delete    : 
+      did:did -> name:str_256 -> obj:dir_entry -> reduce_obj_nlink:unit -> (unit,t)m;
 
-    (* unsafe_delete    : str_256 -> (unit,t)m;  *)
-    (* for when we are sure nothing changes in the dir, ie the dir is locked *)
+    set_times : did:did -> times -> (unit,t)m;
+    get_times : did:did -> (times,t)m;
 
-    delete    : name:str_256 -> obj:dir_entry -> (unit,t)m;
+    set_parent: did:did -> parent:did -> (unit,t)m; (** NOTE inter-object link *)
+    get_parent: did:did -> (did,t)m;
 
-    ls_create : unit -> (dh,t)m;
-    (* FIXME we probably need to track dhs; probably move this to dhs_ops? *)
-
-    set_times : times -> (unit,t)m;
-    get_times : unit -> (times,t)m;
-
-    set_parent: did -> (unit,t)m; (** NOTE inter-object link *)
-    get_parent: unit -> (did,t)m;
-
-    sync: unit -> (unit,t)m; (** used in rename *)
+    sync: did:did -> (unit,t)m; (** used in rename *)
   }
   (** NOTE the parent field is an inter-object link and so needs to be
      updated atomically *)
   (** NOTE no nlink for dirs, but POSIX probably requires it? *)
-
-
-  (* val read_symlink: sid -> (str_256,t)m *)
-  (* FIXME we probably want path res to return an sid, which could be
-     just a string of course *)
 
   type rename_case =
     | Rename_file_missing of {
@@ -135,13 +154,22 @@ module S1(S0:S0) = struct
 
   (* NOTE lookup failures for did and fid are dealt with in the monad *)
   type dirs_ops = {
-    find   : did -> (dir_ops,t)m;
+    (* find   : did -> (dir_ops,t)m; *)
     delete : did -> (unit,t)m;
 
     create : parent:did -> name:str_256 -> times:times -> (unit,t)m; (* was create_dir *)
 
     rename: rename_case -> (unit,t)m;
 
+  }
+
+
+  (** {2 Dir handles} *)
+
+  type dhs_ops = {
+    opendir  : did -> (dh,t)m;
+    readdir  : dh -> (str_256 list,t) m;
+    closedir : dh -> (unit,t)m;
   }
 
 
@@ -158,26 +186,25 @@ module S1(S0:S0) = struct
   end
 
   type file_ops = {
-    pread     : foff:int -> len:int -> buf:buf -> boff:int -> ((int,pread_err)result, t) m;
-    pwrite    : foff:int -> len:int -> buf:buf -> boff:int -> ((int,pwrite_err)result, t) m;
-    truncate  : int -> (unit,t)m;
-    get_sz    : unit -> (int,t)m;
-    set_times : times -> (unit,t)m;
-    get_times : unit -> (times,t)m;
-    get_nlink : unit -> (int,t)m;
-    set_nlink : int -> (unit,t)m;  (** NOTE inter-object *)
+    pread     : fid:fid -> foff:int -> len:int -> buf:buf -> boff:int -> ((int,pread_err)result, t) m;
+    pwrite    : fid:fid -> foff:int -> len:int -> buf:buf -> boff:int -> ((int,pwrite_err)result, t) m;
+    truncate  : fid:fid -> int -> (unit,t)m;
+    get_sz    : fid:fid -> (int,t)m;
+    set_times : fid:fid -> times -> (unit,t)m;
+    get_times : fid:fid -> (times,t)m;
+    get_nlink : fid:fid -> (int,t)m;
+    set_nlink : fid:fid -> int -> (unit,t)m;  (** NOTE inter-object *)
 
-    sync      : unit -> (unit,t)m;
+    sync      : fid:fid -> (unit,t)m;
   }
   (** NOTE the nlink field is derived from inter-object links, and so
       needs to be updated atomically *)
 
   type files_ops = {
-    find        : fid -> (file_ops,t)m;
-    (* create_raw  : fid -> times -> (unit,t)m; (\* was create *\) *)
-    delete      : fid -> (unit,t)m;
-    create : parent:did -> name:str_256 -> times:times -> (unit,t)m;
-    (* FIXME rename to create *)
+    delete: fid -> (unit,t)m;
+    create: 
+      parent:did -> name:str_256 -> times:times -> (unit,t)m;
+    (* NOTE files.create: parent is a dir, and we don't have nlinks for dirs *)
 
     create_symlink:
       parent:did -> name:str_256 -> times:times -> contents:str_256 -> (unit,t)m
@@ -209,9 +236,17 @@ module S2(S0:S0) = struct
   module type T2 = sig
     val root_did: did
 
+    val dir: dir_ops
+
     val dirs: dirs_ops
 
+    val file: file_ops
+
     val files: files_ops
+
+    val dir_handles: dhs_ops
+
+    val locks: (tid,dir_entry,t)lock_ops
 
     val resolve_path: resolve_path
 
@@ -263,9 +298,9 @@ module Make(S0:S0) = struct
       | Ok x -> return (Ok x)
       | Error _e ->
         (* e is a path resolution error, so we need to map it to a
-           standard error FIXME this probably belongs with the
+           standard error FIXME ? this probably belongs with the
            path_resolution code *)
-        return (Error `Error_other) (* FIXME *)
+        return (Error `Error_path_resolution)
 
     (* FIXME not sure about Always in following two funs *)
     let resolve_file_path (path:path) : ((fid,exn_)result,t) m =
@@ -291,47 +326,58 @@ module Make(S0:S0) = struct
        add a "follow" flag? optional? FIXME how does parent/name interact
        with symlinks? FIXME perhaps keep parent/name, but allow another
        layer ot deal with follow; FIXME not sure about `If_trailing_slash  *)
-    let unlink path =
+    let unlink ~tid path =
       resolve_path ~follow_last_symlink:`If_trailing_slash path >>=| fun rpath ->
       let { parent_id=pid; comp=name; result; _ } = rpath in
       match result with
       | Missing -> err `Error_no_entry
       | _ ->
+        (* lock parent and validate pthres *)
+        let objs = [Did pid] in
+        locks.lock ~tid ~objs >>= fun () -> 
         let obj = resolved_component_to_object result in
-        dirs.find pid >>= fun dir_ops ->
-        dir_ops.delete ~name ~obj >>= fun () ->
-        (* FIXME need to atomically reduce nlink by 1 for obj... or assume dir_ops does it *)
-        ok ()
+        dir.find ~did:pid name >>= fun v -> 
+        match v=Some obj with 
+        | false -> 
+          (* something changed... back out *)
+          locks.unlock ~tid ~objs >>= fun () ->
+          err `Error_concurrent_modification
+        | true ->           
+          dir.delete ~did:pid ~name ~obj ~reduce_obj_nlink:() >>= fun () ->
+          locks.unlock ~tid ~objs >>= fun () ->
+          ok ()
 
     (* FIXME meta changes for parent and child *)
-    let mkdir path : ((unit,'e5)result,'t) m =
+    let mkdir ~tid path : ((unit,'e5)result,'t) m =
       resolve_path ~follow_last_symlink:`If_trailing_slash path >>=| fun rpath ->
       let { parent_id=parent; comp=name; result; trailing_slash=_ } = rpath in
       match result with
-      | Missing ->
-        mk_stat_times () >>= fun times ->
-        dirs.create ~parent ~name ~times >>= fun () ->
-        ok ()
+      | Missing -> begin
+          (* lock parent, validate missing *)
+          let objs = [Did parent] in
+          locks.lock ~tid ~objs >>= fun () -> 
+          dir.find ~did:parent name >>= function
+          | None ->         
+            mk_stat_times () >>= fun times ->
+            dirs.create ~parent ~name ~times >>= fun () ->
+            locks.unlock ~tid ~objs >>= fun () ->
+            ok ()
+          | _ -> 
+            (* concurrent modification *)
+            locks.unlock ~tid ~objs >>= fun () ->
+            err `Error_concurrent_modification
+        end
       | _ -> err `Error_exists
 
     (* FIXME update atim *)
     let opendir path =
       resolve_dir_path path >>=| fun did ->
-      dirs.find did >>= fun dir ->
-      dir.ls_create () >>= fun dh ->
+      dir_handles.opendir did >>= fun dh ->
       ok dh
 
-    let readdir (_dh:dh) = failwith ""
-(*
-      dh#ls_kvs () |> fun kvs ->
-      (* FIXME add str_256 conversion to_string to str_256 intf; this is ugly *)
-      (* FIXME move finished to fs_shared *)
-      let kvs = List.map (fun ( (k:str_256),_v) -> (k :> string) ) kvs in
-      dh#ls_step () >>= fun {finished} ->
-      ok (kvs,Tjr_minifs.Minifs_intf.{finished}) (* FIXME move to fs_shared *)
-*)
-    let closedir _ = ok ()
-    (* FIXME should we record which dh are valid? ie not closed *)
+    let readdir (dh:dh) = dir_handles.readdir dh >>= fun xs -> ok xs
+
+    let closedir dh = dir_handles.closedir dh >>= fun () -> ok ()
 
     let create path =
       resolve_path ~follow_last_symlink:`If_trailing_slash path >>=| fun rpath ->
@@ -340,7 +386,6 @@ module Make(S0:S0) = struct
       | Missing ->
         mk_stat_times () >>= fun times ->
         files.create ~parent ~name ~times >>= fun () ->
-        (* FIXME this should update parent.nlink, or assume files.create does this *)
         ok ()
       | _ -> err `Error_exists
 
@@ -351,82 +396,77 @@ module Make(S0:S0) = struct
 
     (* FIXME must account for reading beyond end of file; FIXME atim *)
     let pread ~fd ~foff ~len ~buf ~boff =
-      files.find fd >>= fun file ->
-      file.pread ~foff ~len ~buf ~boff
+      file.pread ~fid:fd ~foff ~len ~buf ~boff
 
     let pwrite ~fd ~foff ~len ~buf ~boff =
-      files.find fd >>= fun file ->
-      file.pwrite ~foff ~len ~buf ~boff
+      (* NOTE no locks *)
+      file.pwrite ~fid:fd ~foff ~len ~buf ~boff
 
     let close _fd = ok ()  (* FIXME record which are open? *)
 
     (* FIXME ddir and sdir may be the same, so we need to be careful to
        always use indirection via did *)
-    let rename spath dpath = begin
+    let rename ~tid spath dpath = begin
       let follow_last_symlink = `Always in
       resolve_path ~follow_last_symlink spath >>=| fun spath ->
       resolve_path ~follow_last_symlink dpath >>=| fun dpath ->
-      (* dirs.find spath.parent_id >>= fun sdir -> *)
       (* NOTE sdir is the container of the spath component *)
       match spath.result with
       | Missing -> err `Error_no_src_entry
       | _ ->
-        (* let scomp = spath.comp in  *)
-        (* dirs.find dpath.parent_id >>= fun ddir -> *)
-        (* NOTE ddir is the container of the dpath component *)
-        (* let dcomp = dpath.comp in *)
-(*
-        (* this looks like it should be an atomic operation, since it affects two objects *)
-        let insert_and_remove id =
-          (* this is the "default" behaviour *)
-          match spath.parent_id = dpath.parent_id with
-          | true ->
-            assert(scomp != dcomp);
-            ddir.insert dcomp id >>= fun () ->
-            ddir.unsafe_delete scomp >>= fun () ->
-            (* update meta on ddir (and sdir!) *)
-            mk_stat_times () >>= fun times ->
-            ddir.set_times times
-          | false ->
-            ddir.insert dcomp id >>= fun () ->
-            mk_stat_times () >>= fun times ->
-            ddir.set_times times >>= fun () ->
-            sdir.unsafe_delete scomp >>= fun () ->
-            sdir.set_times times
-        in
-*)
         match spath.result,dpath.result with
         (* FIMXE following for symlinks *)
         | Sym _,_ | _,Sym _ -> (
             Printf.printf "impossible %s\n%!" __LOC__;
             exit_1 "FIXME shouldn't happen - follow=`Always")
 
-        | File fid,Missing ->
-          mk_stat_times () >>= fun times ->
-          (* FIXME locks, validate pathres;  *)
-          (* FIXME dirs.rename should sync src and dst then do the update atomically *)
-          dirs.rename (Rename_file_missing {
-              times;
-              src=(spath.parent_id,spath.comp,fid);
-              dst=(dpath.parent_id,dpath.comp)
-            }) >>= fun () ->
-          ok ()
+        | File fid,Missing -> begin
+            let objs = [Fid fid; Did spath.parent_id;Did dpath.parent_id] in
+            locks.lock ~tid ~objs >>= fun () -> 
+            dir.find ~did:spath.parent_id spath.comp >>= fun r1 -> 
+            dir.find ~did:dpath.parent_id dpath.comp >>= fun r2 -> 
+            match (r1 = Some (Fid fid)) && (r2 = None) with
+            | false -> 
+              locks.unlock ~tid ~objs >>= fun () ->
+              err `Error_concurrent_modification
+            | true -> 
+              mk_stat_times () >>= fun times ->
+              (* FIXME dirs.rename should sync src and dst then do the update atomically *)
+              dirs.rename (Rename_file_missing {
+                  times;
+                  src=(spath.parent_id,spath.comp,fid);
+                  dst=(dpath.parent_id,dpath.comp)
+                }) >>= fun () ->
+              locks.unlock ~tid ~objs >>= fun () ->
+              ok ()
+          end
 
-        | File fid1,File fid2 -> begin match fid1=fid2 with
+        | File fid1,File fid2 -> begin 
+            match fid1=fid2 with
             | true -> ok ()
             | false ->
-              (* FIXME locks, validate pathres *)
-              (* FIXME dirs.rename should sync src and dst then do the update atomically *)
-              (* FIXME dirs.rename should update nlink etc; and
-                 presumably to keep this consistent we need to sync
-                 the meta for the objs to disk *)
-              mk_stat_times () >>= fun times ->
-              dirs.rename (Rename_file_file {
-                  times;
-                  src=(spath.parent_id,spath.comp,fid1);
-                  dst=(dpath.parent_id,dpath.comp,fid2)
-                }) >>= fun () ->
-              ok ()
+              let objs = [Fid fid1; Fid fid2; Did spath.parent_id; Did dpath.parent_id] in
+              locks.lock ~tid ~objs >>= fun () ->
+              dir.find ~did:spath.parent_id spath.comp >>= fun r1 -> 
+              dir.find ~did:dpath.parent_id dpath.comp >>= fun r2 -> 
+              match (r1 = Some (Fid fid1)) && (r2 = Some (Fid fid2)) with
+              | false -> 
+                locks.unlock ~tid ~objs >>= fun () ->
+                err `Error_concurrent_modification
+              | true -> 
+
+                (* FIXME dirs.rename should sync src and dst then do the update atomically *)
+                (* FIXME dirs.rename should update nlink etc; and
+                   presumably to keep this consistent we need to sync
+                   the meta for the objs to disk *)
+                mk_stat_times () >>= fun times ->
+                dirs.rename (Rename_file_file {
+                    times;
+                    src=(spath.parent_id,spath.comp,fid1);
+                    dst=(dpath.parent_id,dpath.comp,fid2)
+                  }) >>= fun () ->
+                locks.unlock ~tid ~objs >>= fun () ->
+                ok ()
           end
 
         | File _fid,Dir _did ->
@@ -482,14 +522,16 @@ module Make(S0:S0) = struct
 
 
     (* FIXME truncate parent name; FIXME also stat *)
-    let truncate path length =
+    let truncate ~tid path length =
       resolve_file_path path >>=| fun fid ->
-      files.find fid >>= fun file ->
       (* FIXME probably lock file before truncate? so the lower level
          doesn't have to worry *)
-      file.truncate length >>= fun () ->
+      let objs = [Fid fid] in
       mk_stat_times () >>= fun times ->
-      file.set_times times >>= fun () ->
+      locks.lock ~tid ~objs >>= fun () -> 
+      file.truncate ~fid length >>= fun () ->
+      file.set_times ~fid times >>= fun () ->
+      locks.unlock ~tid ~objs >>= fun () ->
       ok ()
 
 
@@ -504,13 +546,12 @@ module Make(S0:S0) = struct
       match result with
       | Missing -> err `Error_no_entry
       | File fid ->
-        files.find fid >>= fun file ->
-        file.get_sz () >>= fun sz ->
-        file.get_times () >>= fun times ->
+        (* NOTE no locks *)
+        file.get_sz ~fid >>= fun sz ->
+        file.get_times ~fid >>= fun times ->
         ok { sz;kind=`File; times }
       | Dir did ->
-        dirs.find did >>= fun dir ->
-        dir.get_times() >>= fun times ->
+        dir.get_times ~did >>= fun times ->
         ok { sz=1; kind=`Dir; times }   (* sz for dir? FIXME size of dir *)
       | Sym (_sid,contents) ->
         ok {sz=String.length contents; times=dummy_times; kind=`Symlink}
@@ -521,7 +562,6 @@ module Make(S0:S0) = struct
       match result with
       | Missing ->
         mk_stat_times () >>= fun times ->
-        (* FIXME add a generic conversion from string, which raises an error in monad?  *)
         let contents = Str_256.make contents in
         files.create_symlink ~parent ~name ~times ~contents >>= fun () ->
         ok ()
@@ -538,13 +578,14 @@ module Make(S0:S0) = struct
 
     (* FIXME what about sync_dir, sync_file ? *)
 
+(*
     (** Export the filesystem operations as a single value *)
     let ops : (_,_,_) Minifs_intf.ops = {
       root;
       unlink;
       mkdir;
       opendir;
-      readdir;
+      readdir=failwith "";  (* FIXME readdir should just return a list surely *)
       closedir;
       create;
       open_;
@@ -560,7 +601,7 @@ module Make(S0:S0) = struct
     }
 
     let _ : (fd,dh,S0.t) Tjr_minifs.Minifs_intf.Ops_type.ops = ops
-
+*)
     (* include these types to allow Make_3 below *)
     type nonrec t = t
     type nonrec dh = dh
@@ -568,7 +609,33 @@ module Make(S0:S0) = struct
   end
 
   (** Make_2, but with a restriction on result sig *)
-  module Make_3(X:T2): T = Make_2(X:T2)
+  (* module Make_3(X:T2): T = Make_2(X:T2) *)
 end
 
 module X = Make
+
+
+        (* let scomp = spath.comp in  *)
+        (* dirs.find dpath.parent_id >>= fun ddir -> *)
+        (* NOTE ddir is the container of the dpath component *)
+        (* let dcomp = dpath.comp in *)
+(*
+        (* this looks like it should be an atomic operation, since it affects two objects *)
+        let insert_and_remove id =
+          (* this is the "default" behaviour *)
+          match spath.parent_id = dpath.parent_id with
+          | true ->
+            assert(scomp != dcomp);
+            ddir.insert dcomp id >>= fun () ->
+            ddir.unsafe_delete scomp >>= fun () ->
+            (* update meta on ddir (and sdir!) *)
+            mk_stat_times () >>= fun times ->
+            ddir.set_times times
+          | false ->
+            ddir.insert dcomp id >>= fun () ->
+            mk_stat_times () >>= fun times ->
+            ddir.set_times times >>= fun () ->
+            sdir.unsafe_delete scomp >>= fun () ->
+            sdir.set_times times
+        in
+*)
