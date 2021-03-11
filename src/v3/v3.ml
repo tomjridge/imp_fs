3ec7ca39bc50efce2ccef5d2e474fa1b3cbb02d8
Tom Ridge <tom.j.ridge@googlemail.com>
2021-03-11 14:53:16 +0000

    wip

--- src/v3/v3.ml
+++ src/v3/v3.ml
@@ -7,8 +7,32 @@
 [@@@warning "-33"]
 
 open V3_abstract
+open Tjr_monad.With_lwt
 
-module S0 : S0 = struct
+module Lru = Lru_two_gen
+
+module Refs_with_dirty_flags = struct
+  type 'a ref = {
+    mutable value: 'a;
+    mutable dirty: bool
+  }
+
+  (** values start off clean *)
+  let ref value = {value;dirty=false}
+
+  let is_dirty x = x.dirty
+
+  let clean x = x.dirty <- false
+
+  (** assigning sets the dirty flag *)
+  let ( := ) r value = r.value <-value; r.dirty <- true
+
+  let (!) r = r.value
+end
+open struct module R = Refs_with_dirty_flags end
+
+
+module S0 (* : S0 *) = struct
   open Bin_prot.Std
 
   type t = lwt
@@ -33,19 +57,117 @@ module S2 = V3_abstract.S2(S0)
 (** What we have to implement *)
 module type T2 = S2.T2
 
-(* FIXME todo 
 module T2 : T2 = struct
 
   let root_did = 0
 
-  (* dir  - implemented using a 2-gen LRU with a dirty flag *)
+  (* dir  - implemented using a 2-gen LRU *)
+
+  let max_sz = 1000
+  let default_cache_size = 1000
 
   module Dir = struct
 
-    (* NOTE we already have ../common/dv3.ml, which does not have
-       parameterization over did; this makes use of wbc_ops, which is
-       not directly implemented by the 2genlru *)
-  
+    (* Directory entries cache *)
+    module S1 = struct
+      type k = str_256
+      type v = dir_entry 
+      type t = S0.t
+      let monad_ops = monad_ops
+    end
+    module Entries_cache = Lru.With_explicit_cache.Make(S1)
+    type entries_cache = Entries_cache.c
+
+    type sqlite_dir_ops = (str_256,dir_entry,t,did) Sqlite_dir.dir_ops
+
+    (* sqlite connection *)
+    let sqlite_dir ~(did:did) : sqlite_dir_ops = failwith "FIXME"[@@warning "-27"]
+
+    type per_dir = {
+      lock           : unit;
+      parent         : did R.ref;
+      times          : times R.ref;
+      entries_cache  : entries_cache;
+      entries        : (str_256,dir_entry,t) Lru.Map_m.map_m;
+      sqldir         : sqlite_dir_ops
+    }
+
+    let per_dir_cache : (did,per_dir)Hashtbl.t = Hashtbl.create default_cache_size
+
+    (* Live directories *)
+    module S0 = struct
+      type k = did
+      type v = per_dir
+      type t = lwt
+      let monad_ops = lwt_monad_ops
+    end
+    module Live_dirs = Lru.With_explicit_cache.Make(S0)
+    let live_dirs = Live_dirs.create_initial_cache ~max_sz
+
+    
+    (* how to recover a directory from the lower layer *)
+    let m3 : _ Lru.map_lower = {
+      find_opt=fun did -> 
+        sqlite_dir ~did |> fun sqldir -> 
+        sqldir.Dv3.get_meta () >>= fun (parent,times) ->         
+        Some {
+          lock=();
+          parent=R.ref parent;
+          times=R.ref times;
+          entries_cache=Entries_cache.create_initial_cache ~max_sz;
+          entries=
+            (*             (* if there is no entry in the entry cache, use sqlite *)
+            let m3 : _ Lru.map_lower = { find_opt = fun k -> per_dir.sqldir.find k } in
+            *)
+            failwith "FIXME";
+          sqldir
+        } |> return
+    }
+
+    let live_dirs_find did = 
+      Live_dirs.ops.find_opt ~m3 ~c:live_dirs did >>= function
+      | None -> 
+        failwith "impossible: did not recognized, but m3 always \
+                  creates a new C2 value in the cache and \
+                  returns from that"
+      | Some per_dir -> return per_dir
+
+    (* FIXME in following we need to take locks *)
+    (* FIXME after each operation, may need a trim of live_dirs, and
+       per-dir entries *)
+    let dir_ops : dir_ops = { 
+      find=(fun ~did s -> 
+          live_dirs_find did >>= fun per_dir -> 
+          per_dir.entries.find_opt s);
+      insert=(fun ~did k v -> 
+          live_dirs_find did >>= fun per_dir -> 
+          per_dir.entries.insert k v);            
+      delete=(fun ~did ~name ~obj:_FIXME ~reduce_obj_nlink:() -> 
+          live_dirs_find did >>= fun per_dir -> 
+          per_dir.entries.delete name);
+      set_times=R.(fun ~did times -> 
+          live_dirs_find did >>= fun per_dir -> 
+          per_dir.times := times; 
+          return ());
+      get_times=R.(fun ~did -> 
+          live_dirs_find did >>= fun per_dir -> 
+          !(per_dir.times) |> return );
+      set_parent=R.(fun ~did ~parent -> 
+          live_dirs_find did >>= fun per_dir -> 
+          per_dir.parent := parent; 
+          return ());
+      get_parent=R.(fun ~did -> 
+          live_dirs_find did >>= fun per_dir -> 
+          !(per_dir.parent) |> return);
+      sync=failwith""
+    }
+
+    end
+
+
+    let dir_cache : (did,dir_cache_entry)
+
+
     type k = did
     type v = (
 
@@ -60,4 +182,3 @@ module T2 : T2 = struct
     in
 
 end
-*)
